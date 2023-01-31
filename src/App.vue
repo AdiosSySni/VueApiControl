85aa237a20d8fa09db94f9f90b00815bcfe8850d
AdiosSyS <62465250+AdiosSySni@users.noreply.github.com>
2023-01-31 17:43:15 +0200

    Add files via upload
    create store

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Comments from './components/Comments.vue';
+import InputForm from './components/InputForm.vue';
+import Resume from './components/Resume.vue';
+
+import {ref, reactive, provide} from 'vue'
+import {commentsStore} from './store/store'
+
+const store = commentsStore()
+provide('commentsStore', store)
+// store.fetchComments()
+// console.log(store.comments)
+
+
+
+</script>
+
+
+<template>
+  <div class="container column">
+    <input-form></input-form>
+    <resume></resume>
+  </div>
+  <comments></comments>
+</template>
+
+<style>
+  .avatar {
+    display: flex;
+    justify-content: center;
+  }
+
+  .avatar img {
+    width: 150px;
+    height: auto;
+    border-radius: 50%;
+  }
+</style>
---
+++ src/components/Comments.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref, reactive, inject} from 'vue'
+const commentsStore = inject('commentsStore')    
+let isLoad = ref(true);
+
+console.log('Check')
+
+function loadComments() {    
+    console.log(commentsStore.comments)
+    commentsStore.fetchComments()
+}
+</script>
+
+<template>
+    <div class="container">
+        <p>
+        <button class="btn primary" @click="loadComments()">Загрузить комментарии</button>
+        </p>
+
+        <div class="card">
+        <h2>Комментарии</h2>
+        <ul class="list">
+            <li class="list-item">
+            <div>
+                <p><strong>test@microsoft.com</strong></p>
+                <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, reiciendis.</small>
+            </div>
+            </li>
+        </ul>
+        </div>
+  </div>
+</template>
